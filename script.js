7ca234b609c8a43a1e91104b48e6ed805d98ef66
YashGunjal <238607689+YashGunjal9@users.noreply.github.com>
2026-09-07 18:12:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Creating objects
+const person = {
+    name: "Yash",
+    age: 20,
+    city: "Sangamner",
+};
+
+// Adding properties object
+//! Dot notation
+person.isUnmarried = true;
+person.country = "India";
+//! Bracket notion
+person["hobby"] = "Coding"
+person["Cources"] = ["HTML", "CSS", "JavaScript"];
+console.log(person);
